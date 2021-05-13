604321e6e49a294c4fbe1448379a2f69a948297d
Rode1o <dumirola.dr@gmail.com>
2021-05-12 19:57:09 -0700

    limiters

--- monty.h
+++ monty.h
@@ -1,6 +1,6 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
-#define LIMITERS " \t"
+#define LIMITERS " \t\n"
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
