2c719d658c84af3aa294dadcc01c78855fca711d
Rode1o <dumirola.dr@gmail.com>
2021-05-13 09:55:19 -0700

    fix msg error

--- _pop.c
+++ _pop.c
@@ -15,7 +15,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	if (!*stack)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(2);
 	}
 
