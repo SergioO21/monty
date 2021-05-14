ff09bfb463cd39ea908dc9b15ca455723bfff85b
Rode1o <dumirola.dr@gmail.com>
2021-05-13 20:01:02 -0700

    brain

---
+++ 2test.m
@@ -0,0 +1,33 @@
+push 1 This 
+#c
+push 2
+push 3
+	# a
+#
+#a
+pint
+nop
+push 4
+swap
+pop
+ #a
+	#
+add
+push 1
+sub
+push 2
+div
+push 12
+mul
+push 9
+mod
+push 0
+push 65
+pchar
+push 66
+push 67
+push 68
+pstr
+rotl
+push 87
+pall
